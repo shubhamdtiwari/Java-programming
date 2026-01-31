1965402131c49d420df3ee08e211cadf531df1b1
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-01-31 16:21:18 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String a[]) 
+    {
+        int numa = 8;
+        int numb = 9;
+        int result = numa + numb ;
+
+         System.out.println(result);
+    }
+}  
