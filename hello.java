a150cb0381623bbfc834dc5c1d1e1de834bc1b07
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-21 23:48:49 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,3 +1,12 @@
-public class hello {
+class Hello
+{
+    public static void main(String a[]) 
+    {
+        int numa = 8;
+        int numb = 9;
+        int result = numa + numb ;
 
-}
+        System.out.println(result);
+    }
+}  
+ 
