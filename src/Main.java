f2db100edbdff579298189c22856535088bdc0aa
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-10 15:40:13 +0530

    Use of System.out.println

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hey how are you?");
         
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
     }
     
 }
