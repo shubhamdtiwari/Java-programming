08ad4249190bfbd55929e43a5f0e0f72e0987ddb
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-10 23:57:14 +0530

    Create Loops.java

---
+++ src/Loops.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Loops {
+    public static void main(String[] args) {
+        
+        // print 5 Hello world 
+
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+
+        /*
+                Syntax of for loops:
+
+                for(initialisation; condition; increment/decrement) {
+                // body
+                }
+        */
+
+             // Q: Print numbers from 1 to 5
+
+
+             for(int num = 1; num <= 5; num++){
+                System.out.println(num);
+             }
+    }
+    
+}
