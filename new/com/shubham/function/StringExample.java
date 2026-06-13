09c4340992a06800931e383bc34d7150b853e8e4
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-14 00:08:25 +0530

    more function

---
+++ new/com/shubham/function/StringExample.java
@@ -0,0 +1,13 @@
+package com.shubham.function;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+       String greeting = "how are you";
+        return greeting;
+    }
+}
--- new/com/shubham/function/Sum.java
+++ new/com/shubham/function/Sum.java
@@ -4,11 +4,34 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
+//        int ans = sum2();
+//        System.out.println(ans);
 
+        int ans = sum3(45, 34);
+        System.out.println(ans);
 
     }
+    //pass the value of numbers when we are calling the method in main()
 
-    void sum() {
+    static int sum3 (int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // return the value
+
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
+//        System.out.println("this will never execute");
+    }
+
+   static void sum() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter number 1: ");
         int num1 = in.nextInt();
