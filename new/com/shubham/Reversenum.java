86628d446cdb1ba053a62b6652317b4dca1c8a88
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-13 12:45:10 +0530

    switch formate

--- new/com/shubham/Reversenum.java
+++ new/com/shubham/Reversenum.java
@@ -1,4 +1,20 @@
 package com.shubham;
 
 public class Reversenum {
+    public static void main(String[] args) {
+
+        int n = 12345;
+
+        int res = 0;
+
+        while(n > 0) {
+            int rem = n % 10;
+            n /= 10;
+
+            res = res * 10 + rem;
+
+        }
+
+        System.out.println(res);
+    }
 }
--- new/com/shubham/Switch.java
+++ new/com/shubham/Switch.java
@@ -1,9 +1,48 @@
-
+package com.shubham;
 
 import java.util.Scanner;
 
 public class Switch{
     static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+
+//        if(fruit.equals("mango")) {
+//            System.out.println("King of fruit");
+//        }
+//        if (fruit.equals("apple")) {
+//            System.out.println("a sweet red fruit");
+//        }
+
+        // for avoiding repeated code we use switch
+
+//        switch (fruit) {
+//            case "Mango":
+//                System.out.println("King of fruits");
+//                break;
+//            case "Apple":
+//                System.out.println("A sweet red fruit");
+//                break;
+//            case "Orange":
+//                System.out.println("Round fruit");
+//                break;
+//            case "Grapes":
+//                System.out.println("small fruit");
+//                break;
+//            default:
+//                System.out.println("Enter a valid fruit");
+//
+//        }
+
+        // new formate
+
+        switch (fruit) {
+            case "Mango" -> System.out.println("King of fruits");
+            case "Apple" -> System.out.println("A sweet red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            case "Grapes" -> System.out.println("small fruit");
+            default -> System.out.println("Enter a valid fruit");
+        }
     }
 }
