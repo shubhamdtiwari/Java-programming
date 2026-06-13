8031483497568232940ef15307f9089fb4ac42db
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-13 17:03:19 +0530

    new switch method

--- new/com/shubham/Switch.java
+++ new/com/shubham/Switch.java
@@ -71,6 +71,10 @@ public class Switch{
                 break;
         }
 
+        switch (day) {
+            case 1,2,3,4,5 -> System.out.println("Weekday");
+            case 6,7 -> System.out.println("Weekend");
+        }
 
     }
 }
