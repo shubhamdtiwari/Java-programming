7dbfb25c925ba5f687980f9ff43473586f399cfd
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-13 16:45:34 +0530

    making switch

--- new/com/shubham/Switch.java
+++ new/com/shubham/Switch.java
@@ -37,12 +37,24 @@ public class Switch{
 
         // new formate
 
-        switch (fruit) {
-            case "Mango" -> System.out.println("King of fruits");
-            case "Apple" -> System.out.println("A sweet red fruit");
-            case "Orange" -> System.out.println("Round fruit");
-            case "Grapes" -> System.out.println("small fruit");
-            default -> System.out.println("Enter a valid fruit");
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of fruits");
+//            case "Apple" -> System.out.println("A sweet red fruit");
+//            case "Orange" -> System.out.println("Round fruit");
+//            case "Grapes" -> System.out.println("small fruit");
+//            default -> System.out.println("Enter a valid fruit");
+//        }
+
+        int day = in.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+
         }
     }
 }
