dedcc3af345b686777bcbd759f5b8a1a2f3e1554
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-14 14:58:53 +0530

    examples of functions

---
+++ new/com/shubham/function/Swap.java
@@ -0,0 +1,24 @@
+package com.shubham.function;
+
+public class Swap {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        // Swap two numbers
+//        int temp = a;
+//        a = b;
+//        b = temp;
+        swap(a, b);
+
+        System.out.println(a + " " + b);
+    }
+
+    static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+
+    }
+}
