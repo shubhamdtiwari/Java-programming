7357051d02dd869bb97fa14d79cf98d5d9b54b07
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-14 17:05:29 +0530

    VarArgs more expm

--- new/com/shubham/function/VarArgs.java
+++ new/com/shubham/function/VarArgs.java
@@ -1,4 +1,20 @@
 package com.shubham.function;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class VarArgs {
+    public static void main(String[] args) {
+//        fun(2, 3, 4, 5, 6, 54, 45,23);
+        multiple(2, 3, "kunal", "Shubham");
+    }
+
+
+    static void multiple(int a, int b, String ...v) {
+
+    }
+
+    static void fun(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
 }
