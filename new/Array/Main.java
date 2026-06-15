234310e6cd678fdd4e1d955cc2f5bb1ca8f814bb
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-15 11:18:05 +0530

    arrays

---
+++ new/Array/Main.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        // Q: store a roll number
+        int a = 19;
+
+        // Q: store a person's name
+        String name = "Shubham Tiwari";
+
+
+        //Q: stores 5 roll numbers
+        int roll1 = 23;
+        int roll2 = 32;
+        int roll3 = 54;
+
+        // syntax
+        // datatype[] variable_name = datatype[size];
+        // store 5 roll numbers
+
+        int[] rnos = new int[5];
+
+        // or directly
+        int[] rnos2 = {23, 12,34, 43};
+
+    }
+}
