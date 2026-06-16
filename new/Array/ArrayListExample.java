8bff46af6c6a41dbf7755d038964b8e483af0d56
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-16 15:41:03 +0530

    ArrayList

---
+++ new/Array/ArrayListExample.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // syntax
+
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        list.add(67);
+        list.add(6754);
+        list.add(677);
+        list.add(667);
+        list.add(567);
+        list.add(1267);
+        list.add(267);
+        list.add(6567);
+        list.add(63427);
+        list.add(6756);
+        list.add(67);
+        list.add(6754);
+        list.add(677);
+        list.add(667);
+        list.add(567);
+        list.add(1267);
+        list.add(267);
+        list.add(6567);
+        list.add(63427);
+        list.add(6756);list.add(67);
+        list.add(6754);
+        list.add(677);
+        list.add(667);
+        list.add(567);
+        list.add(1267);
+        list.add(267);
+        list.add(6567);
+        list.add(63427);
+        list.add(6756);
+
+        System.out.println(list);
+    }
+}
