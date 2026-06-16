56708e2e8180d23a11f2c82e58be4aa454ebac54
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-16 15:50:14 +0530

    ArrayList exaples

--- new/Array/ArrayListExample.java
+++ new/Array/ArrayListExample.java
@@ -1,9 +1,12 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         // syntax
 
         ArrayList<Integer> list = new ArrayList<>(10);
@@ -17,27 +20,27 @@ public class ArrayListExample {
         list.add(267);
         list.add(6567);
         list.add(63427);
-        list.add(6756);
-        list.add(67);
-        list.add(6754);
-        list.add(677);
-        list.add(667);
-        list.add(567);
-        list.add(1267);
-        list.add(267);
-        list.add(6567);
-        list.add(63427);
-        list.add(6756);list.add(67);
-        list.add(6754);
-        list.add(677);
-        list.add(667);
-        list.add(567);
-        list.add(1267);
-        list.add(267);
-        list.add(6567);
-        list.add(63427);
-        list.add(6756);
+
+
+//        System.out.println(list.contains(53454));
 
         System.out.println(list);
+        list.set(0, 99);
+
+        list.remove(2);
+
+        System.out.println(list);
+
+        //input
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+
+        // get item at any index
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i)); // pass index here, list[index] syntax not work here
+
+        }
+//        System.out.println(list);
     }
 }
