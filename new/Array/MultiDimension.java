45c01ef7d1e4b5ccbfe6d23e4bdc35ae0ef7faec
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-16 11:52:49 +0530

    2D arrays

---
+++ new/Array/MultiDimension.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+         /*
+             1 2 3
+             4 6 7
+             8 9 5
+          */
+
+//        int[][] arr = new int[3][3];
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+    }
+}
