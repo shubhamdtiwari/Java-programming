f96d1916837628d92cc13ead5aacfc86aaeae42b
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-17 00:22:15 +0530

    reverse

--- new/Array/Swap.java
+++ new/Array/Swap.java
@@ -1,10 +1,24 @@
 package Array;
 
+import java.util.Arrays;
+
 public class Swap {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 32};
-        swap(arr, 1, 3);
+        int[] arr = {1, 2, 3, 4, 32, 8};
+//        swap(arr, 1, 3);
 
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while ( start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
     }
 
     static void swap(int[] arr, int index1, int index2) {
