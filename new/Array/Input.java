10a630f9625026d629402ba2ea757ba416e743e6
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-16 11:40:09 +0530

    modify arrays

--- new/Array/Input.java
+++ new/Array/Input.java
@@ -9,6 +9,7 @@ public class Input {
     Scanner in = new Scanner(System.in);
     int[] arr = new int[5];
 
+    // array of primitives
     arr[0] = 23;
     arr[1] = 213;
     arr[2] = 223;
@@ -18,12 +19,12 @@ public class Input {
     System.out.println(arr[3]);
 
     // input using for loops
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = in.nextInt();
-    }
+//    for (int i = 0; i < arr.length; i++) {
+//      arr[i] = in.nextInt();
+//    }
 
 
-    System.out.println(Arrays.toString(arr));
+//    System.out.println(Arrays.toString(arr));
 
 //    for (int i = 0; i < arr.length; i++) {
 //      System.out.println(arr[i] + " ");
@@ -35,8 +36,20 @@ public class Input {
 
 //    System.out.println(arr[5]); // index out of bound error
 
+    // array of objects
 
+    String[] str = new String[4];
+    for (int i = 0; i < str.length; i++) {
+      str[i] = in.next();
+    }
 
+    System.out.println(Arrays.toString(str));
+
+    // modify
+
+    str[1] = "Shubh";
+
+    System.out.println(Arrays.toString(str));
 
     }
 }
