f1d34bf7f240f78329e9b11bd748a5ccd6a5e700
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-17 23:56:01 +0530

    Create ArraySearch.java

---
+++ practise/new/Array/ArraySearch.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class ArraySearch {
+
+    public static void main(String[] args) {
+        
+    }
+
+    // search in the array: return the index if item found
+    // otherwise if item not found return -1
+    static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        // run a for loop
+        for
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+}
