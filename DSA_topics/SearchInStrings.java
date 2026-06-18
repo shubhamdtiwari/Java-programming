e959193ab7d4be2eaf1f8f35df4d4bbcddb50356
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-18 17:36:37 +0530

    Create SearchInStrings.java

---
+++ DSA_topics/SearchInStrings.java
@@ -0,0 +1,22 @@
+package DSA_topics;
+
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "Shubham";
+        char target = 'u';
+        System.out.println(search(name, target));
+
+    }
+    static boolean search(String str, char target) {
+        if(str.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if(target == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
