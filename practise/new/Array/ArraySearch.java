f29692ab6ab811b860922e79cec5b9a6a6185965
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-18 15:00:10 +0530

    other type of outputs

--- practise/new/Array/ArraySearch.java
+++ practise/new/Array/ArraySearch.java
@@ -9,6 +9,38 @@ public class ArraySearch {
         System.out.println(ans);
     }
 
+    // saerch the target and return true or flase
+ static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+
+        // run a for loop
+        for(int element : arr) {
+            if(element == target) {
+                return true;
+            }
+        }
+        return false;
+    }  
+
+    // saerch the target and return the element
+ static int linearSearch2(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        // run a for loop
+        for(int element : arr) {
+            if(element == target) {
+                return element;
+            }
+        }
+        return Integer.MAX_VALUE;
+    }  
+
+
+
     // search in the array: return the index if item found
     // otherwise if item not found return -1
     static int linearSearch(int[] arr, int target) {
@@ -25,6 +57,8 @@ public class ArraySearch {
             }
         
         }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
         return -1;
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 }
