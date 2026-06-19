08f115327bcf67bc146c81bf02fb465246630158
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-19 19:10:03 +0530

    SearchInRange

---
+++ DSA_topics/LinearSeachAlgo/SearchInRange.java
@@ -0,0 +1,29 @@
+package DSA_topics.LinearSeachAlgo;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] arr = {18, 12, 3, 65, 677, 30, 35};
+        int target = 3;
+        System.out.println(linearSearch(arr, target, 1, 5));
+        
+    }
+
+    static int linearSearch(int[] arr, int target, int start, int end) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        // run a for loop
+        for(int index = start; index <= end; index++) {
+            // cheak for element at every index if it is = target
+            int element = arr[index];
+            if(element == target) {
+                return index;
+            }
+        
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return -1;
+    }    
+}
