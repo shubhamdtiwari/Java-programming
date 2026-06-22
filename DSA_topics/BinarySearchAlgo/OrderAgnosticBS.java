35bdfd77ad16b7e899fe63134e105b34f1a1c52f
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-06-22 20:52:49 +0530

    Create OrderAgnosticBS.java

---
+++ DSA_topics/BinarySearchAlgo/OrderAgnosticBS.java
@@ -0,0 +1,40 @@
+package DSA_topics.BinarySearchAlgo;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+        
+    }
+    static int OrderAgnosticsBS(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        if(e > s) { // incresing
+
+            while( e > s) {
+                int mid = s + (s - e) / 2;
+
+                if(arr[mid] > target) {
+                    e = mid - 1;
+                }else if( arr[mid] < target) {
+                    s = mid + 1;
+                }else{
+                    return mid;
+                }
+
+            }
+        }else if(e < s) {
+            while( e >= s) {
+                int mid = s + (s - e) / 2;
+
+                if(arr[mid] > target) {
+                    e = mid + 1;
+                }else if( arr[mid] < target) {
+                    s = mid - 1;
+                }else{
+                    return mid;
+                }
+        }
+    }
+    return -1;
+    
+}
+}
