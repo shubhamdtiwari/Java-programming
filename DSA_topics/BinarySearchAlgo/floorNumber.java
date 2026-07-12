a18046bb935e85adcd6bbbb25dc85636614f5268
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-13 02:29:30 +0530

    Update floorNumber.java

--- DSA_topics/BinarySearchAlgo/floorNumber.java
+++ DSA_topics/BinarySearchAlgo/floorNumber.java
@@ -3,7 +3,7 @@ package DSA_topics.BinarySearchAlgo;
 public class floorNumber {
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 9, 14, 17, 18};
-        int target = 15;
+        int target = 1;
 
         int ans = floorNum(arr, target);
         System.out.println(ans);
@@ -12,6 +12,12 @@ public class floorNumber {
     static int floorNum(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
+
+         // if the target element is less than the less number in the array
+
+        if(target < arr[0]){
+            return -1;
+        }
 
         // floor number means number less than or equal to the given number
 
