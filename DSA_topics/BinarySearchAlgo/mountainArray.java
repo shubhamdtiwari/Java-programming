c464c3262160ab0aaf31da35324ea9444b183651
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-18 00:22:08 +0530

    Update mountainArray.java

--- DSA_topics/BinarySearchAlgo/mountainArray.java
+++ DSA_topics/BinarySearchAlgo/mountainArray.java
@@ -7,9 +7,14 @@ public class mountainArray {
         
     }
 
-    int search(int[] arr){
+    int search(int[] arr, int target){
         int peak = peakIndexInMountainArray(arr);
-        int firstTry = binarySearch(arr, target, )
+        int firstTry = orderAgnosticBS(arr, target, 0, peak);
+
+        if(firstTry != -1){
+            return firstTry;
+        }
+        return orderAgnosticBS(arr, target, peak + 1, arr.length - 1);
 
     }
         
@@ -28,8 +33,40 @@ public class mountainArray {
                 start = mid + 1;
             }
         }
-       
+       return start;
     }
 
-    static int bimarysearch(int[] arr, int target)
+    static int orderAgnosticBS(int[] arr, int target, int s, int e) {
+       
+
+
+        // find whether the array is sorted in ascending and decending order
+        boolean isAsc = arr[s] < arr[e];
+
+         while (s <= e) {
+            // find the middle element
+            // int mid = (start + end) / 2;
+            // maybe (start + end) exceed the range of int in java
+            int mid = s + (e - s) / 2; // better way to calc mid
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            if(isAsc) {
+                  if( target < arr[mid]) {
+                    e = mid - 1;
+                 }else {
+                    s = mid + 1;
+                 }
+                }else {
+                if( target > arr[mid]) {
+                    e = mid - 1;
+                 }else {
+                    s = mid + 1;
+                }
+            }
+         }
+         return -1;
+
+    }
 }
