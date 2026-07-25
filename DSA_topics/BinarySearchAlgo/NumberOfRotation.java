5172d784b96c44a8ae6e08c48d2e20e8d90d1a64
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-25 23:41:02 +0530

    Update NumberOfRotation.java

--- DSA_topics/BinarySearchAlgo/NumberOfRotation.java
+++ DSA_topics/BinarySearchAlgo/NumberOfRotation.java
@@ -1,6 +1,9 @@
 public class NumberOfRotation {
     public static void main(String[] args) {
         int[] arr = {15, 18, 2, 3, 6, 12};
+
+        int ans = rotationCount(arr);
+        System.out.println(ans);
     }
 
     static int rotationCount(int[] arr){
@@ -8,7 +11,14 @@ public class NumberOfRotation {
         int e = arr.length - 1;
 
         while(s <= e){
+            int m = s + ( e - s) / 2;
+
+            if(arr[m] > arr[m + 1] && arr[m] < arr[m - 1]){
+                return m;
+            }
+
             
         }
+        return -1;
     }
 }
