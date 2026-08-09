15903e3d0611799c841ab8993bb764dc2f8c8a74
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-09 23:38:33 +0530

    Update linearSearch.java

--- DSA_topics/SearchingIn2DArrays/linearSearch.java
+++ DSA_topics/SearchingIn2DArrays/linearSearch.java
@@ -18,4 +18,13 @@ public class linearSearch {
        
     }
 
-
+     static int[] search(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+}
