2367a8892da465d7a6369737bba578a887fd58f1
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-09 23:38:25 +0530

    Update linearSearch.java

--- DSA_topics/SearchingIn2DArrays/linearSearch.java
+++ DSA_topics/SearchingIn2DArrays/linearSearch.java
@@ -1,1 +1,21 @@
+package DSA_topics.SearchingIn2DArrays;
+
+import java.util.Arrays;
+
+public class linearSearch {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {23, 4, 1},
+            {18, 12, 3, 9},
+            {78, 99, 34, 56},
+            {18, 12}
+        };
+
+    int target = 34;
+
+        int[] ans = search(arr, target); // formate of return value {row, col}
+        System.out.println(Arrays.toString(ans));
+       
+    }
+
 
