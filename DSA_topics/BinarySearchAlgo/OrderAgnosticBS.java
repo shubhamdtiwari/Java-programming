2a9b7ccb7e5209a1454c89346a8800148a56ba2b
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-10 23:55:51 +0530

    Update OrderAgnosticBS.java

--- DSA_topics/BinarySearchAlgo/OrderAgnosticBS.java
+++ DSA_topics/BinarySearchAlgo/OrderAgnosticBS.java
@@ -1,1 +1,11 @@
+package DSA_topics.BinarySearchAlgo;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+
+    int[] arr = {3,4, 5, 8,13,24,56,78,87};
+    int target = 78;
+    System.out.println(orderAgnosticBS(arr, target));
+        
+    }
 
