567f831cd8df7641ebbf252627cf020ba8fd290c
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-10 23:56:06 +0530

    Update OrderAgnosticBS.java

--- DSA_topics/BinarySearchAlgo/OrderAgnosticBS.java
+++ DSA_topics/BinarySearchAlgo/OrderAgnosticBS.java
@@ -8,4 +8,21 @@ public class OrderAgnosticBS {
     System.out.println(orderAgnosticBS(arr, target));
         
     }
+    static int orderAgnosticBS(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+
+
+        // find whether the array is sorted in ascending and decending order
+        boolean isAsc = arr[s] < arr[e];
+
+         while (s <= e) {
+            // find the middle element
+            // int mid = (start + end) / 2;
+            // maybe (start + end) exceed the range of int in java
+            int mid = s + (e - s) / 2; // better way to calc mid
+            if(arr[mid] == target){
+                return mid;
+            }
+
 
