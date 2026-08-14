397ea0a7e76e9569bb1b82e1bceff1ad2606144e
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-15 01:50:45 +0530

    Update searchInSortedMatrix.java

--- DSA_topics/SearchingIn2DArrays/searchInSortedMatrix.java
+++ DSA_topics/SearchingIn2DArrays/searchInSortedMatrix.java
@@ -4,4 +4,11 @@ public class searchInSortedMatrix {
     public static void main(String[] args) {
         
     }
+
+    static int[] search(int[][] matrix, int target){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        if(rows == )
+    }
 }
