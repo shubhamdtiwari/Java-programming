2f9c7fee046f7f1d798d604dec0a447878779d33
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-15 23:49:42 +0530

    Update searchInSortedMatrix.java

--- DSA_topics/SearchingIn2DArrays/searchInSortedMatrix.java
+++ DSA_topics/SearchingIn2DArrays/searchInSortedMatrix.java
@@ -5,10 +5,20 @@ public class searchInSortedMatrix {
         
     }
 
+    static int[] binarySearch(int[][] matrix, int row, int cStart, int cEnd, int target){
+        while(cStart <= cEnd){
+            int mid = cStart + (cEnd - cStart) / 2;
+
+            if()
+        }
+    }
+
     static int[] search(int[][] matrix, int target){
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        if(rows == )
+        if(rows == 1){
+
+        }
     }
 }
