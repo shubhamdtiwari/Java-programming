d6e9f078f9cda554f653489936b319c0033d3524
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-08-17 17:09:08 +0530

    Bubble Sort Algo

---
+++ DSA_topics/BubbleSortAlgo/algo.java
@@ -0,0 +1,30 @@
+package DSA_topics.BubbleSortAlgo;
+
+import java.util.Arrays;
+
+public class algo {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 2, 6, 7, 8, 1, 1, 33, 31};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+        // run the steps n-1 times
+
+        for(int i = 0; i < arr.length; i++){
+            // for each step, max item will come at the last respective index
+
+            for (int j = 1; j < arr.length - i; j++) {
+                // swap if the item is smaller than previous item
+
+                if(arr[j] < arr[j -1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
