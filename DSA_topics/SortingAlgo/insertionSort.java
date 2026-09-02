fe4f6f96b009d3686ff0251e69470f741a2d3693
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-02 20:41:56 +0530

    selectionSort

--- DSA_topics/SortingAlgo/insertionSort.java
+++ DSA_topics/SortingAlgo/insertionSort.java
@@ -1,7 +1,32 @@
 package DSA_topics.SortingAlgo;
 
+import java.util.Arrays;
+
 public class insertionSort {
     public static void main(String[] args) {
+        int[] arr = { 5, 3, 2, 6, 7, 8, 1, 1, 33, 31 };
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
 
     }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
 }
