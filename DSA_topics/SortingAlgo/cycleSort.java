cd4562e87998e4a6362a9f1109b37b5695710c50
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-04 00:31:36 +0530

    adding cyclic Algo

--- DSA_topics/SortingAlgo/cycleSort.java
+++ DSA_topics/SortingAlgo/cycleSort.java
@@ -4,18 +4,27 @@ import java.util.Arrays;
 
 public class cycleSort {
     public static void main(String[] args) {
-        int[] arr = { 5, 3, 4, 2, 1 };
+        int[] arr = { 5, 3, 4, 2, 1, 7, 10, 9, 6, 8, 11 };
         cycle(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void cycle(int[] arr) {
 
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+
     }
 
-    static void swap(int[] arr, int first, int second){
+    static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
-        arr[second] = temp
+        arr[second] = temp;
     }
 }
