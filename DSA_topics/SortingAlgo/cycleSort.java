2144b6be5c5dbe180b512b24199608e388d82452
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-04 00:01:18 +0530

    adding swap

--- DSA_topics/SortingAlgo/cycleSort.java
+++ DSA_topics/SortingAlgo/cycleSort.java
@@ -1,5 +1,21 @@
 package DSA_topics.SortingAlgo;
 
-public class cycleSort {
+import java.util.Arrays;
 
+public class cycleSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 4, 2, 1 };
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cycle(int[] arr) {
+
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp
+    }
 }
