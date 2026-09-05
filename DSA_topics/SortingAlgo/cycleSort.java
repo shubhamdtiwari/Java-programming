dfdf0d7bb003e60f3f85443af6d80c00a5963e64
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-05 20:21:44 +0530

    Adding link

--- DSA_topics/SortingAlgo/cycleSort.java
+++ DSA_topics/SortingAlgo/cycleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class cycleSort {
     public static void main(String[] args) {
-        int[] arr = { 5, 3, 4, 2, 1, 7, 10, 9, 6, 8, 11 };
+        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
         cycle(arr);
         System.out.println(Arrays.toString(arr));
     }
