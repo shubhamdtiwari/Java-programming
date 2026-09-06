2ed01d0fe69a44765c0570c952daf13d137251e0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-06 20:35:28 +0530

    prettyprintng

---
+++ DSA_topics/StringConcept/preetyprinting.java
@@ -0,0 +1,8 @@
+package DSA_topics.StringConcept;
+
+public class preetyprinting {
+    public static void main(String[] args) {
+        float a = 345.2446f;
+        System.out.printf("Formatted number is %.2f", a);
+    }
+}
