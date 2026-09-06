48ce0a9e6a4322d2623515bbf2f5c640409cb79f
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-06 23:41:19 +0530

    prettyprintng

--- DSA_topics/StringConcept/preetyprinting.java
+++ DSA_topics/StringConcept/preetyprinting.java
@@ -3,6 +3,8 @@ package DSA_topics.StringConcept;
 public class preetyprinting {
     public static void main(String[] args) {
         float a = 345.2446f;
-        System.out.printf("Formatted number is %.2f", a);
+        // System.out.printf("Formatted number is %.2f", a);
+
+        System.out.printf("Hello my name is %s and I am %s", "Shubh", "Cool");
     }
 }
