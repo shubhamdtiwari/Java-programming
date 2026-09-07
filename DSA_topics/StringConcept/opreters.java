1f3476ffb2549362fdd52cf49949bca903628768
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-07 15:11:00 +0530

    operators in string

---
+++ DSA_topics/StringConcept/opreters.java
@@ -0,0 +1,9 @@
+package DSA_topics.StringConcept;
+
+public class opreters {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b'); // 195
+        System.out.println("a" + "b"); // ab
+        System.out.println((char)('a' + 3)); // d
+    }
+}
