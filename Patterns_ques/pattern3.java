51322b664943cffd06584d1a484c0d8a338f91f8
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-10 23:50:01 +0530

    pattern 3

---
+++ Patterns_ques/pattern3.java
@@ -0,0 +1,17 @@
+package Patterns_ques;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        p3(5);
+    }
+
+    static void p3(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row + 1; col++) {
+                System.out.print("* ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+}
