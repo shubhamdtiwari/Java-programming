6cc3710e3538f1a518dfb1ae5ab64323197929f0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-10 23:54:21 +0530

    pattern 4

--- Patterns_ques/pattern4.java
+++ Patterns_ques/pattern4.java
@@ -8,7 +8,7 @@ public class pattern4 {
     static void p4(int n) {
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= row; col++) {
-                System.out.print(col);
+                System.out.print(col + " ");
             }
             // when one row is printed, we need to add a newline
             System.out.println();
