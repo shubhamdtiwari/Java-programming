681da7755d4df5e8586e35b8c94fea1dbcd349b1
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-12 16:37:43 +0530

    other from of pattern 31

--- Patterns_ques/pattern31.java
+++ Patterns_ques/pattern31.java
@@ -10,26 +10,16 @@ package Patterns_ques;
 
 public class pattern31 {
     public static void main(String[] args) {
-        otherp31(4);
+        p31(4);
     }
 
-    // 4 4 4 4 4 4 4 4 4
-    // 4 3 3 3 3 3 3 3 4
-    // 4 3 2 2 2 2 2 3 4
-    // 4 3 2 1 1 1 2 3 4
-    // 4 3 2 1 0 1 2 3 4
-    // 4 3 2 1 1 1 2 3 4
-    // 4 3 2 2 2 2 2 3 4
-    // 4 3 3 3 3 3 3 3 4
-    // 4 4 4 4 4 4 4 4 4
-
-    static void otherp31(int n) {
+    static void p31(int n) {
         int orgn = n;
         n = 2 * n;
 
-        for (int row = 0; row < n - 1; row++) {
-            for (int col = 0; col < n - 1; col++) {
-                int atEveryIndex = orgn - Math.min(Math.min(row, col), Math.min(n - col - 1, n - row - 1));
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex = orgn - Math.min(Math.min(row, col), Math.min(n - col, n - row));
                 System.out.print(atEveryIndex + " ");
             }
             System.out.println();
