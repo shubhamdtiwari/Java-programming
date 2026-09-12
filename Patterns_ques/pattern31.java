3804cdf2fbaecefa0221524566c81de1d1124eb5
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-12 20:02:06 +0530

    Pattern 31

--- Patterns_ques/pattern31.java
+++ Patterns_ques/pattern31.java
@@ -1,19 +1,43 @@
 package Patterns_ques;
 
-// 31.      4 4 4 4 4 4 4  
-//          4 3 3 3 3 3 4   
-//          4 3 2 2 2 3 4   
-//          4 3 2 1 2 3 4   
-//          4 3 2 2 2 3 4   
-//          4 3 3 3 3 3 4   
-//          4 4 4 4 4 4 4 
-
 public class pattern31 {
+
     public static void main(String[] args) {
         p31(4);
     }
 
+    // 4 4 4 4 4 4 4
+    // 4 3 3 3 3 3 4
+    // 4 3 2 2 2 3 4
+    // 4 3 2 1 2 3 4
+    // 4 3 2 2 2 3 4
+    // 4 3 3 3 3 3 4
+    // 4 4 4 4 4 4 4
+
     static void p31(int n) {
+        int orgn = n;
+        n = 2 * n;
+
+        for (int row = 0; row < n - 1; row++) {
+            for (int col = 0; col < n - 1; col++) {
+                int atEveryIndex = orgn - Math.min(Math.min(row, col), Math.min(n - col - 2, n - row - 2));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // 4 4 4 4 4 4 4 4 4
+    // 4 3 3 3 3 3 3 3 4
+    // 4 3 2 2 2 2 2 3 4
+    // 4 3 2 1 1 1 2 3 4
+    // 4 3 2 1 0 1 2 3 4
+    // 4 3 2 1 1 1 2 3 4
+    // 4 3 2 2 2 2 2 3 4
+    // 4 3 3 3 3 3 3 3 4
+    // 4 4 4 4 4 4 4 4 4
+
+    static void otherp31(int n) {
         int orgn = n;
         n = 2 * n;
 
