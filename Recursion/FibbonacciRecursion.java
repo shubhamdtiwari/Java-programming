4d7f46192361843f32caa76168ef37025554beb0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-13 20:13:22 +0530

    Using recursion in Fibbonacci

---
+++ Recursion/FibbonacciRecursion.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class FibbonacciRecursion {
+    public static void main(String[] args) {
+        System.out.println(fibo(50));
+    }
+
+    static int fibo(int n) {
+        // base condition
+        if (n < 2) {
+            return n;
+        }
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
