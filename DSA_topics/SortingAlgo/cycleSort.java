bde9f63faf14297878fa6431265f692a6f366b92
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-09-23 00:01:09 +0530

    cycle sort

--- DSA_topics/SortingAlgo/cycleSort.java
+++ DSA_topics/SortingAlgo/cycleSort.java
@@ -1,2 +1,30 @@
+package DSA_topics.SortingAlgo;
 
+import java.util.Arrays;
+
+public class cycleSort {
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cycle(int[] arr) {
+
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
